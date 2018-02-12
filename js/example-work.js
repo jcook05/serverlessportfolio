a1b54d14b20785541eb81bfec805e0e703173071
John Cook <root@DESKTOP-HSQ9D3L.localdomain>
2018-02-11 23:07:04 -0700

    react works

---
+++ js/example-work.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ExampleWork extends React.Component {
+
+    render() {
+
+        return (
+
+            <section> It works!</section>
+
+        )
+    }
+
+}
+
+export default ExampleWork
--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-import 'react.dom'
+import React from 'react';
+import ReactDom from 'react-dom';
+import ExampleWork from './example-work';
+
+
+ReactDom.render(<ExampleWork />, document.getElementById('example-work'));
+
 
 console.log("Loaded react-dom")
 console.log("Webpack Worked!")
-
+console.log("Expand your mind")
